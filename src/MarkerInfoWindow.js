6f64770e05e4a430afb87bc978cdceb29f5736e5
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2018-07-25 19:40:10 +0200

    working on showing the Markers returned from API

--- src/MarkerInfoWindow.js
+++ src/MarkerInfoWindow.js
@@ -4,22 +4,37 @@ import { Marker, InfoWindow } from "react-google-maps"
 require("recompose");
 
 class MarkerInfoWindow extends React.Component {
-    render(){
-        return (
-            <Marker
-                position={{ lat: 25.7188346, lng: 32.6550816 }}
-                onClick={this.props.onToggleOpen}
-                >
-    
-                {console.log(this.props.venue[0].name)}
-                {this.props.isOpen &&
-                <InfoWindow onCloseClick={this.props.onToggleOpen}>
 
-                     {/* Added this here so we could grab the name of the first venue in the list*/}
-                     <h1>{this.props.venue[0].name}</h1>
-                     
-                </InfoWindow>}
-            </Marker>
+    
+
+    render(){
+        console.log(this.props.venue)
+        this.props.venue.map(venue => {
+            console.log(venue)
+
+            const position = {lat: 25.7188346, lng: 32.6550816}
+        })
+        return (
+
+            <div>
+                {this.props.venue.map(marker => {
+                    
+                    <Marker
+                    position={{lat: marker.location.lat, lng: marker.location.lng}}
+                    onClick={this.props.onToggleOpen}
+                    >
+                
+                    {console.log(marker.location.lat)}
+        
+                    {console.log(this.props.venue[0].name)}
+                    {this.props.isOpen &&
+                    <InfoWindow onCloseClick={this.props.onToggleOpen}>
+                        <h1>{this.props.venue[0].name}</h1>
+                        
+                    </InfoWindow>}
+                </Marker>
+                })}
+            </div>
         )
     }
 }
